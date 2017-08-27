cf2c56d3ee28196f37e4d8f6fa6586085d549148
Adriaan de Groot <groot@kde.org>
2017-08-27 16:08:43 +0200

    Import CMake modules from Quick-DER
     - Massage the macros to get files from ARPA2CM install dir as well
     - Add ignores, top-level driver Makefile

---
+++ toolchain/MacroAddUninstallTarget.cmake
@@ -0,0 +1,33 @@
+# ADD_UNINSTALL_TARGET()
+#    Add custom target 'uninstall' that removes all the files
+#    installed by this build (not recommended by CMake devs though).
+#
+# Add an uninstall target, as described on the CMake wiki.
+# Include this file, then call add_uninstall_target().
+# Requires a top-level cmake/ directory containing this
+# macro file and a cmake_uninstall.cmake.in.
+
+macro(add_uninstall_target)
+	# uninstall target, source of uninstall template from
+	# least to most specific.
+	set(_uninstall_in "")
+	foreach(_uninstall_in_candidate
+		"${ARPA2CM_TOOLCHAIN_DIR}/cmake_uninstall.cmake.in"
+		"${CMAKE_CURRENT_LIST_DIR}/cmake_uninstall.cmake.in"
+		"${CMAKE_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in")
+		if(EXISTS ${_uninstall_in_candidate})
+			set(_uninstall_in ${_uninstall_in_candidate})
+		endif()
+	endforeach()
+	if(NOT _uninstall_in)
+		message(FATAL_ERROR "No cmake_uninstall.cmake.in was found.")
+	endif()
+
+	configure_file(
+		"${_uninstall_in}"
+		"${CMAKE_BINARY_DIR}/cmake_uninstall.cmake"
+		IMMEDIATE @ONLY)
+
+	add_custom_target(uninstall
+		COMMAND ${CMAKE_COMMAND} -P ${CMAKE_BINARY_DIR}/cmake_uninstall.cmake)
+endmacro()
---
+++ toolchain/MacroCreateConfigFiles.cmake
@@ -0,0 +1,87 @@
+# CREATE_CONFIG_FILES(<packagename>)
+#    Call this macro to generate CMake and pkg-config configuration
+#    files from templates found in the top-level source directory.
+#
+# Most ARPA2-related components write configuration-information
+# files and install them. There are two flavors:
+#
+#   - CMake config-info files (<foo>Config.cmake and <foo>ConfigVersion.cmake)
+#   - pkg-config files (<foo>.pc)
+#
+# The macro create_config_files() simplifies this process
+# by using named template files for all three output files.
+# Pass a package name (e.g. "Quick-DER") to the macro, and
+# the source files (e.g. <file>.in for the files named above
+# will be taken from the top-level contrib/{cmake,pkgconfig}
+# source directories.
+#
+# As an (un)special case, the ConfigVersion file may be taken from
+# the cmake/ directory, since there is nothing particularly special
+# for that file (as opposed to the other files, which need to
+# specify paths, dependencies, and other things).
+
+# Copyright 2017, Adriaan de Groot <groot@kde.org>
+#
+# Redistribution and use is allowed according to the terms of the two-clause BSD license.
+#    https://opensource.org/licenses/BSD-2-Clause
+#    SPDX short identifier: BSD-2-Clause
+
+macro (create_config_file_internal _packagename _bstype _filename)
+	# Find the .in files
+	set(_configfile_in "")
+	foreach(_configfile_in_candidate
+		"${ARPA2CM_TOOLCHAIN_DIR}/${_filename}.in"
+		"${CMAKE_CURRENT_LIST_DIR}/${_filename}.in"
+		"${CMAKE_CURRENT_LIST_DIR}/${_packagename}${_filename}.in"
+		"${CMAKE_SOURCE_DIR}/${_bstype}/${_filename}.in"
+		"${CMAKE_SOURCE_DIR}/${_bstype}/${_packagename}${_filename}.in"
+		"${PROJECT_SOURCE_DIR}/contrib/${_bstype}/${_filename}.in"
+		"${PROJECT_SOURCE_DIR}/contrib/${_bstype}/${_packagename}${_filename}.in"
+		)
+		if(EXISTS ${_configfile_in_candidate})
+			set(_configfile_in ${_configfile_in_candidate})
+		endif()
+	endforeach()
+	if(NOT _configfile_in)
+		message(FATAL_ERROR "No ${_filename}.in was found.")
+	endif()
+
+	# Substitute in real values for the placeholders in the .in files,
+	# create the files in the build tree, and install them.
+	configure_file (${_configfile_in}
+		"${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${_packagename}${_filename}" @ONLY)
+endmacro()
+
+macro (create_config_files _packagename)
+	export (PACKAGE ${_packagename})
+	# The CMake configuration files are written to different locations
+	# depending on the host platform, since different conventions apply.
+	if (WIN32 AND NOT CYGWIN)
+		set (DEF_INSTALL_CMAKE_DIR CMake)
+	else ()
+		set (DEF_INSTALL_CMAKE_DIR lib/cmake/${_packagename})
+	endif ()
+	set (INSTALL_CMAKE_DIR ${DEF_INSTALL_CMAKE_DIR} CACHE PATH
+		"Installation directory for CMake files")
+
+	# Calculate include/ relative to the installed place of the config file.
+	file (RELATIVE_PATH REL_INCLUDE_DIR "${CMAKE_INSTALL_PREFIX}/${INSTALL_CMAKE_DIR}"
+		"${CMAKE_INSTALL_PREFIX}/include")
+	file (RELATIVE_PATH REL_LIB_DIR "${CMAKE_INSTALL_PREFIX}/${INSTALL_CMAKE_DIR}"
+		"${CMAKE_INSTALL_PREFIX}/lib")
+	set (CONF_INCLUDE_DIRS "\${${_packagename}_CMAKE_DIR}/${REL_INCLUDE_DIR}")
+	set (CONF_LIB_DIRS "\${${_packagename}_CMAKE_DIR}/${REL_LIB_DIR}")
+	set (_conf_version ${${_packagename}_VERSION})
+
+	create_config_file_internal(${_packagename} cmake Config.cmake)
+	create_config_file_internal(${_packagename} cmake ConfigVersion.cmake)
+	create_config_file_internal(${_packagename} pkgconfig .pc)
+
+	install (FILES
+		"${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${_packagename}Config.cmake"
+		"${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${_packagename}ConfigVersion.cmake"
+		DESTINATION "${INSTALL_CMAKE_DIR}" COMPONENT dev)
+	install (FILES
+		"${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${_packagename}.pc"
+		DESTINATION "lib/pkgconfig/")
+endmacro ()
